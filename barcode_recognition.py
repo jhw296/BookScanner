cf51951f57dd6d4c70b2afa618ef266b6105f45a
jhw296 <johyewon4601@gmail.com>
2023-06-11 18:08:41 +0900

    Fix barcode_recognition in main.py

---
+++ barcode_recognition.py
@@ -0,0 +1,83 @@
+import cv2
+import requests
+import os
+import numpy as np
+# import pytesseract import Output
+import pytesseract
+from pyzbar.pyzbar import decode
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from PIL import Image
+
+global title, author, barcode_cnt
+title = None
+author = None
+barcode_cnt = 0
+
+def barcorde_recognition():
+    global title, author, barcode_cnt
+    load_dotenv()
+    barcode_cnt = 0
+    
+    cap = cv2.VideoCapture(0)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if not ret:
+            continue
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        barcodes = decode(gray) # 바코드 인식
+
+        for barcode in barcodes:
+            barcode_cnt += 1
+            x, y, w, h = barcode.rect
+            barcode_data = barcode.data.decode("utf-8")
+            barcode_type = barcode.type
+            barcode_text = '%s (%s)' % (barcode_data, barcode_type)
+            
+            # print("Barcode data:", barcode_data)
+            
+            # barcode bounding box 생성
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
+            cv2.putText(frame, barcode_text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
+
+            # ISBN 바코드인 경우 (978, 979로 시작하는 13자리 숫자)
+            if (barcode_data.startswith("978") or barcode_data.startswith("979")) and len(barcode_data) == 13:
+                isbn = barcode_data
+                key = os.environ.get('key')
+                url = f"https://www.nl.go.kr/NL/search/openApi/search.do?key={key}&detailSearch=true&isbnOp=isbn&isbnCode={isbn}"
+                response = requests.get(url)
+                soup = BeautifulSoup(response.content, 'html.parser') # html 파일
+                
+                title = soup.find('title_info').text
+                author = soup.find('author_info').text
+
+                if title is None:
+                    title = "Not Found"
+                    
+                # 바코드가 지속적으로 10번 이상 감지될 경우 해당 도서 정보 출력
+                if barcode_cnt >= 10:
+                    path = './img/snapshot_' + str(title) + '.jpg'
+                    cv2.imwrite(path, frame)
+                    cap.release()
+                    book_info()
+                    
+            else:
+                print("Not an ISBN barcode")
+
+        cv2.imshow('Barcode reader', frame)
+        # cv2.imshow('grayscale', gray) 
+        
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
+def book_info():
+    global title, author
+    # print("Title: ", title)
+    # print("Author: ", author)
+    
+    return title, author
